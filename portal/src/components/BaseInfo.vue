76e6a1581db31029c04fcc3ee36dc9fd8d908239
codejie <codejie@sohu.com>
2020-12-01 22:58:42 +0800

    try Base vue

---
+++ portal/src/components/BaseInfo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="base">
+    BaseInfo Table
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ portal/src/views/Base.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="base">
+    <h1><b>BaseInfo</b></h1>
+    <BaseInfo/>
+  </div>
+</template>
+
+<script>
+import BaseInfo from '@/components/BaseInfo.vue'
+export default {
+  name: 'Base',
+  components: {
+    BaseInfo
+  }
+}
+</script>
